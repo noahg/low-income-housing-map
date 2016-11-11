d6d38ded0ea84ef65299c6eabe20f1e7db400988
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2016-11-10 17:26:46 -0800

    add LD shapes and put on the map

--- js/housing-map-leaflet.js
+++ js/housing-map-leaflet.js
@@ -1,38 +1,5 @@
 mapboxAccessToken = 'pk.eyJ1Ijoibm9haGciLCJhIjoiaDZOQVlFayJ9.sKF3imccqs6EJE57Y3j2SA';
 
-var geojson = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-77.03238901390978,38.913188059745586]
-      },
-      "properties": {
-        "title": "Mapbox DC",
-        "description": "1714 14th St NW, Washington DC",
-        "marker-color": "#fc4353",
-        "marker-size": "large",
-        "marker-symbol": "monument"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-122.414, 37.776]
-      },
-      "properties": {
-        "title": "Mapbox SF",
-        "description": "155 9th St, San Francisco",
-        "marker-color": "#fc4353",
-        "marker-size": "large",
-        "marker-symbol": "harbor"
-      }
-    }
-  ]
-};
 
 var map = L.map('map').setView([47.27, -120.82], 8);
 
@@ -44,4 +11,4 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
   id: 'mapbox.light'
 }).addTo(map);
 
-L.geoJson(geojson).addTo(map);
+var geojson = L.geoJson(ldshapes).addTo(map);
