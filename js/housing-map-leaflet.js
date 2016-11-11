cfb1239d1148db83fb3a1bda09b4107401f9a4a4
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2016-11-10 17:31:47 -0800

    add hover events

--- js/housing-map-leaflet.js
+++ js/housing-map-leaflet.js
@@ -11,4 +11,49 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
   id: 'mapbox.light'
 }).addTo(map);
 
-var geojson = L.geoJson(ldshapes).addTo(map);
+
+function style(feature) {
+    return {
+        //fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.7
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 4,
+        color: '#333',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(ldshapes, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
